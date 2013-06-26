40b52031bf3a0374526fc6b4b94d8c6c87e91908
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-27 01:14:58 +0400

    adding calculation

--- src/dim.ml
+++ src/dim.ml
@@ -1,5 +1,12 @@
+open String
+
+open Item
 (* open Point *)
 
-let calc data =
+let calc (n_items, capacity, items) =
+  let sorted = List.rev (List.sort Item.compare items) in
+  let strs = List.map Item.string sorted in
+  let str = String.concat "\n" strs in
+  Printf.printf "strings:\n%s\n" str;
   ()
 
