1c10ce15de2d14b431d2853bd6882b7a39147ddc
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-27 00:29:49 +0400

    read input file

--- src/opts.ml
+++ src/opts.ml
@@ -1,4 +1,6 @@
 open Arg
+open Pcre
+
 open Dim
 
 let read_data verbose ifile = ()
@@ -15,11 +17,53 @@ let get_pars verbose ifile =
     (Printf.printf "no input file exist: %s\n" !ifile;
      assert false)
 
+let read_file file =
+  let lines = ref [] in
+  let chan = open_in file in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done;
+    []
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines
+
+let regexp = Pcre.regexp "(\\d+)\\s+(\\d+)"
+
+let parse_line line =
+  try
+    match Pcre.extract_all ~rex:regexp ~full_match:false line
+    with
+    | [|[|s1; s2|]|] ->
+      Some (int_of_string s1, int_of_string s2)
+    | _ ->
+      None
+  with
+  | Not_found ->
+    None
+  | Failure "int_of_string" ->
+    None
+
+let parse_lines lines =
+  let Some (n_items, capacity) = parse_line (List.hd lines) in
+  let items = List.map parse_line (List.tl lines) in
+  let filtered = List.filter (function
+    | Some _ -> true
+    | None -> false)
+    items in
+  assert (n_items = List.length filtered);
+  (n_items, capacity, filtered)
+
+let read_data verbose file =
+  let lines = read_file file in
+  parse_lines lines
+
 let run () =
   let ifile = ref "" in
   let verbose = ref 1 in
   let _ = get_pars verbose ifile in
-  let in_data = read_data verbose ifile in
+  let in_data = read_data verbose !ifile in
   let res = Dim.calc in_data in
   print_result res
 
