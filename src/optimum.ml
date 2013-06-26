dd1f5a0d2e60ddc544f04420f1e519cbc1d68375
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-27 01:34:12 +0400

    add optimum module

---
+++ src/optimum.ml
@@ -0,0 +1,33 @@
+open Item
+
+let add_whole acc acc_int capacity item =
+  let w = Item.weight item in
+  let v = Item.value item in
+  let new_acc = acc + v in
+  let new_acc_int = new_acc in
+  let new_capacity = capacity - w in
+  new_acc, new_acc_int, new_capacity
+
+let add_part acc acc_int capacity h =
+  let w = Item.weight item in
+  let v = Item.value item in
+  let new_acc = acc + v in
+  let new_acc_int = new_acc in
+  let new_capacity = capacity - w in
+  new_acc, new_acc_int, new_capacity
+
+let rec adding acc acc_int capacity = function
+  | [] -> acc, acc_int
+  | h :: t ->
+    let w = Item.weight h in
+    let new_acc, new_acc_int, new_capacity =
+      if w > capacity
+      then add_part
+      else add_whole acc acc_int capacity h
+    in
+    adding new_acc new_acc_int new_capacity t
+
+let calc (n_items, capacity, items) =
+  let sorted = List.rev (List.sort Item.compare items) in
+  ()
+
