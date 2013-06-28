a2e4f8517fa9e18f5ef1a370450d56e7c0574460
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-28 09:51:26 +0400

    add backtrack module

---
+++ src/backtrack.ml
@@ -0,0 +1,33 @@
+
+let do_not_use_item cur_c item_idx acc =
+  let new_item_idx = item_idx - 1 in
+  let _ = Point.set_item acc item_idx 0 in
+  (new_item_idx, cur_c, acc)
+
+let use_item cur_c c item_idx items acc =
+  let (_, wei) = Point.get_item item_idx items in
+  let new_item_idx = item_idx - 1 in
+  let new_cur_c = cur_c - wei in
+  let _ = Point.set_item acc item_idx 1 in
+  (new_item_idx, new_cur_c, acc)
+
+let get_new_item cur_c c item_idx items acc table =
+  let cur_val = Point.get_point item_idx cur_c c table in
+  let prev2_val = Point.get_point (item_idx - 1) cur_c c table in
+  if cur_val = prev2_val
+  then do_not_use_item cur_c item_idx acc
+  else use_item cur_c c item_idx items acc
+
+let rec backtrack_aux cur_c c item_idx items acc table =
+  if item_idx < 0
+  then acc
+  else let (new_x, new_y, new_acc) = get_new_item
+         cur_c c item_idx items acc table in
+       backtrack_aux new_y c new_x items new_acc table
+
+let backtrack capacity items table =
+  let x = (Array.length items) - 1 in
+  let y = capacity - 1 in
+  let acc = Array.make x 0 in
+  backtrack_aux y capacity x items acc table
+
