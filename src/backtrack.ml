e1c0b6bc636be51c8486dc31b6e27efa8297b618
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-29 20:50:57 +0400

    use appropriate point api

--- src/backtrack.ml
+++ src/backtrack.ml
@@ -13,8 +13,8 @@ let use_item cur_c c item_idx items acc =
   (new_item_idx, new_cur_c, acc)
 
 let get_new_item cur_c c item_idx items acc table =
-  let cur_val = Point.get_point item_idx cur_c c table in
-  let prev2_val = Point.get_point (item_idx - 1) cur_c c table in
+  let cur_val = Point.get_point2 item_idx cur_c c table in
+  let prev2_val = Point.get_point2 (item_idx - 1) cur_c c table in
   if cur_val = prev2_val
   then do_not_use_item cur_c item_idx acc
   else use_item cur_c c item_idx items acc
@@ -26,18 +26,26 @@ let rec backtrack_aux cur_c c item_idx items acc table =
          cur_c c item_idx items acc table in
        backtrack_aux new_y c new_x items new_acc table
 
-let backtrack capacity items table =
-  let x = (Array.length items) - 1 in
-  let y = capacity - 1 in
-  let acc = Array.make (Array.length items) 0 in
-  backtrack_aux y capacity x items acc table
+let backtrack capacity items = function
+  | None ->
+    None
+  | Some table ->
+    let x = (Array.length items) - 1 in
+    let y = capacity - 1 in
+    let acc = Array.make (Array.length items) 0 in
+    let used_items = backtrack_aux y capacity x items acc table in
+    Some used_items
 
-let print items =
-  let rec print_aux x items =
-    if x >= Array.length items
-    then Printf.printf "\n"
-    else let _ = Printf.printf "%d " items.(x) in
-         print_aux (x + 1) items
-  in
-  print_aux 0 items
+let print = function
+  | None ->
+    Printf.eprintf "backtracking not possible: no data available\n";
+    Printf.printf "\n"
+  | Some items0 ->
+    let rec print_aux x items =
+      if x >= Array.length items
+      then Printf.printf "\n"
+      else let _ = Printf.printf "%d " items.(x) in
+           print_aux (x + 1) items
+    in
+    print_aux 0 items0
 
