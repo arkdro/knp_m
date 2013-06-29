c303368a6806edbc16f4f00bfcc02f552b52912d
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-29 20:49:48 +0400

    make points for 1 and 2 dimention tables

--- src/point.ml
+++ src/point.ml
@@ -1,6 +1,16 @@
 exception Wrong_coord of string * int
 
-let get_point x y height table =
+let get_point1 x line =
+  if x < 0
+  then 0
+  else
+    Bigarray.Array1.get line x
+
+let set_point1 x value line =
+  if x < 0 then raise (Wrong_coord ("x < 0 on set_point", x));
+  Bigarray.Array1.set line x value
+
+let get_point2 x y height table =
   if x < 0
   then 0
   else if y < 0
@@ -9,7 +19,7 @@ let get_point x y height table =
     (* table.{x, y} *)
     Bigarray.Array2.get table x y
 
-let set_point x y height value table =
+let set_point2 x y height value table =
   if x < 0 then raise (Wrong_coord ("x < 0 on set_point", x));
   if y < 0 then raise (Wrong_coord ("y < 0 on set_point", y));
   (* table.{x, y} <- value *)
@@ -28,10 +38,19 @@ let dump_table tab =
   let h = Bigarray.Array2.dim2 tab in
   for x = 0 to w - 1 do
     for y = 0 to h - 1 do
-      let value = get_point x y h tab in
+      let value = get_point2 x y h tab in
       Printf.eprintf "%d " value
     done;
     Printf.eprintf "\n"
   done;
   Printf.eprintf "\n"
+
+let dump_line line =
+  let len = Bigarray.Array1.dim line in
+  for i = 0 to len - 1 do
+    let value = get_point1 i line in
+    Printf.eprintf "%d " value
+  done;
+  Printf.eprintf "\n"
+
 
